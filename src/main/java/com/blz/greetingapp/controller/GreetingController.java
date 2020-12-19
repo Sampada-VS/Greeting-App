e0ef18361f737c36e9e0e600be830f9abd3f7d84
Sampada-VS <samssv1@gmail.com>
2020-12-20 02:30:28 +0530

    Added: greeting controller to get simple hello world message as default

--- src/main/java/com/blz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/blz/greetingapp/controller/GreetingController.java
@@ -2,7 +2,6 @@ package com.blz.greetingapp.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,7 @@ public class GreetingController {
 	private final AtomicLong counter=new AtomicLong();
 		
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name") String name) {
+	public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
 		return new Greeting(counter.incrementAndGet(),
 							String.format(template, name));
 	}
